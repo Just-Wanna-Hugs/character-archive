fd9655cc23ae7abc968194232d7226f501ffd406
Just-Wanna-Hugs <pavlov12kiril@gmail.com>
2026-08-11 10:51:36 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE = "character-archive-v1";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(["./", "./index.html", "./manifest.json", "./icon.svg"]))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request).then(response => {
+        const clone = response.clone();
+        caches.open(CACHE).then(cache => cache.put(e.request, clone));
+        return response;
+      }).catch(() => caches.match("./index.html"));
+    })
+  );
+});
